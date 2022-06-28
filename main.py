6b45c498fb3461e960e0a3e60678644ae812d68a
Celtuzed <76615478+Celtuzed@users.noreply.github.com>
2022-06-28 19:15:45 +0300

    Add scripts

---
+++ main.py
@@ -0,0 +1,30 @@
+def fix_marks(schoolkid='Фролов Иван'):
+    child = Schoolkid.objects.filter(full_name__contains=schoolkid).get()
+    marks = Mark.objects.filter(schoolkid=child)
+    child_bad_marks = marks.filter(points__lte=3)
+    for mark in range(child_bad_marks.count()):
+        child_bad_mark = child_bad_marks.first()
+        child_bad_mark.points = 5
+        child_bad_mark.save()
+
+
+def remove_chastisements(schoolkid='Фролов Иван'):
+    child = Schoolkid.objects.filter(full_name__contains=schoolkid).get()
+    chastisements = Chastisement.objects.filter(schoolkid=child)
+    for chastisement in chastisements:
+        chastisement.delete()
+
+
+def create_commendation(schoolkid='Фролов Иван', subject_title='Музыка'):
+    commendation_text=[
+        "Молодец!",
+        "Отлично!",
+        "Хорошо!",
+        "Гораздо лучше, чем я ожидал!",
+        "Здорово!"
+    ]
+    child = Schoolkid.objects.filter(full_name__contains=schoolkid).get()
+    subject = Subject.objects.filter(title=subject_title, year_of_study=child.year_of_study).first()
+    lesson = Lesson.objects.filter(subject=subject, year_of_study=child.year_of_study, group_letter=child.group_letter).first()
+    Commendation.objects.create(schoolkid=child, teacher=lesson.teacher, subject=lesson.subject, created=lesson.date, text=random.choice(commendation_text))
+
